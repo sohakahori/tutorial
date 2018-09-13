407926996342ad08a9ff416ef77adfac14d53225
sohakahori <uu138994@RLSUU138994M01.local>
2018-09-13 13:39:17 +0900

    10ユーザーの更新・表示・削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :show, :updatek, :index]
+  before_action :logged_in_user, only: [:edit, :show, :updatek, :index, :destroy]
   before_action :correct_user, only: [:edit, :show, :update]
   before_action :admin_user, only: [:destroy]
 
